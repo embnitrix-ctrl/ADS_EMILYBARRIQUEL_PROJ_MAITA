dd2eebfdf1deb549d66f174eb99692b2e1d2e06b
USUARIO <USUARIO@ec2-54-232-189-113.sa-east-1.compute.amazonaws.com>
2026-05-20 16:03:37 -0300

    VVVV

---
+++ src/main/java/br/upf/projetomaita/entity/AndarEntity.java
@@ -0,0 +1,10 @@
+package br.upf.projetomaita.entity;
+
+import jakarta.persistence.Entity;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class AndarEntity implements Serializable {
+    
+}
