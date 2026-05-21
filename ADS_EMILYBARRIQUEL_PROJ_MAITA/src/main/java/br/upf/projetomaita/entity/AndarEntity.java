b4b31470eec653fce5a7347dda0c315428306a0c
Emily de Morais Barriquel <197705@upf.br>
2026-05-21 20:35:08 -0300

    ja commitei no net beans mas não vaiiiiiiiii por la

---
+++ ADS_EMILYBARRIQUEL_PROJ_MAITA/src/main/java/br/upf/projetomaita/entity/AndarEntity.java
@@ -0,0 +1,53 @@
+package br.upf.projetomaita.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "andar")
+public class AndarEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @OneToMany(mappedBy = "andar")
+    private List<ApartamentoEntity> apartamentos;
+
+    public AndarEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<ApartamentoEntity> getApartamentos() {
+        return apartamentos;
+    }
+
+    public void setApartamentos(List<ApartamentoEntity> apartamentos) {
+        this.apartamentos = apartamentos;
+    }
+}
