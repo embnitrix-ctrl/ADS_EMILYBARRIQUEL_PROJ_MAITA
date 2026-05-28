da182126264a89c342a69bc62bd9f6feaeca137a
USUARIO <emily@gmail.com>
2026-05-28 10:27:13 -0300

    tntando subir novamente as entidades

---
+++ src/main/java/br/upf/ads_emilybarriquel_proj_maita/entity/ManutencaoEntity.java
@@ -0,0 +1,77 @@
+package br.upf.ads_emilybarriquel_proj_maita.entity;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "manutencao")
+public class ManutencaoEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataAbertura;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataFechamento;
+
+    @Column(nullable = false)
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "apartamento_id", nullable = false)
+    private ApartamentoEntity apartamento;
+
+    public ManutencaoEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(Date dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public Date getDataFechamento() {
+        return dataFechamento;
+    }
+
+    public void setDataFechamento(Date dataFechamento) {
+        this.dataFechamento = dataFechamento;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public ApartamentoEntity getApartamento() {
+        return apartamento;
+    }
+
+    public void setApartamento(ApartamentoEntity apartamento) {
+        this.apartamento = apartamento;
+    }
+}
